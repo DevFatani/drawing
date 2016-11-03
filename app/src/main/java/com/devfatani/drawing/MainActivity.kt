df9313baf31c17ffd21d8666ecd2994aa97aefdd
devfatani <i_fatani@outlook.com>
2016-11-03 11:46:11 +0300

    just log

--- app/src/main/java/com/devfatani/drawing/MainActivity.kt
+++ app/src/main/java/com/devfatani/drawing/MainActivity.kt
@@ -2,11 +2,13 @@ package com.devfatani.drawing
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.setContentView(R.layout.activity_main)
+        Log.v(toString(), "v3")
     }
 }
